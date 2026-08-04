6d06af0c6d358bc5c14a1b7b3e48f25752e9e2b3
Martin Kotrulev <martinbkotr@Martins-MacBook-Pro.local>
2026-08-04 10:24:54 +0300

    Credit control email: Amount Due showed the invoice total instead of the open balance

--- account_credit_control_dunning_fees/model/credit_control_communication.py
+++ account_credit_control_dunning_fees/model/credit_control_communication.py
@@ -10,3 +10,7 @@ class CreditCommunication(models.Model):
     def _get_total_due(self):
         balance_field = "credit_control_line_ids.balance_due_total"
         return sum(self.mapped(balance_field))
+
+    @api.model
+    def _get_line_balance_due(self, line):
+        return line.balance_due_total
